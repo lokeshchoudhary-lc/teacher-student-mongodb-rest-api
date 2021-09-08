1a1d1d005e6581491e18b46d629b8ef6afc8264a
Lokesh Choudhary <lokeshchoudhary8949@gmail.com>
2021-09-08 22:17:31 +0530

    feat: Add mongoose

---
+++ db/index.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const mongouri = process.env.MONGODB_URI;
+const dbname = process.env.DB_NAME;
+mongoose.connect(mongouri, {
+  dbName: dbname,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+mongoose.connection.on('connected', () => console.log('Connected to database'));
+mongoose.connection.on('error', (err) => console.log(err.message));
+mongoose.connection.on('disconnected', () =>
+  console.log('Disconnected to database')
+);
+
+process.on('SIGINT', async () => {
+  await mongoose.connection.close();
+  process.exit(0);
+});
